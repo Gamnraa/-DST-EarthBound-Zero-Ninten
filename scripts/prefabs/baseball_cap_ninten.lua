954fb178ffe84781ea5695789dd894ec646e4ccc
Gamnraa <ltaz1466@gmail.com>
2023-02-13 20:31:28 -0500

    Update Baseball Cap to Ness' version

--- scripts/prefabs/baseball_cap_ninten.lua
+++ scripts/prefabs/baseball_cap_ninten.lua
@@ -3,9 +3,26 @@ local assets =
    Asset("ANIM", "anim/baseball_cap_ninten.zip"),
 }
 
+
+local function equip_ness_check(owner)
+	if owner.AnimState:GetCurrentFacing() == 2 then 
+		owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_ness_l")
+	elseif owner.AnimState:GetCurrentFacing() == 0 then
+		owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_ness_r")
+	end
+end
+
 local function onequip(inst, owner)
+	
 	if owner.prefab == "gramninten" then
-        owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_off")
+		if owner.components.skinner.skin_name == "ms_gramninten_summer" then
+			owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_off_alt")
+		else
+			owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_off")
+		end
+	elseif owner.prefab == "gramness" then
+		owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat_ness_l")
+		owner:ListenForEvent("locomote", equip_ness_check)
 	else
 		owner.AnimState:OverrideSymbol("swap_hat", "baseball_cap_ninten", "swap_hat")
     end
@@ -15,16 +32,11 @@ local function onequip(inst, owner)
     owner.AnimState:Hide("HAIR_NOHAT")
     owner.AnimState:Hide("HAIR")
 
-    --[[if owner:HasTag("player") then
+    if owner:HasTag("player") then
         owner.AnimState:Hide("HEAD")
-        owner.AnimState:Show("HEAD_HAT")
-    end]]
-	if owner:HasTag("player") then
-		owner.AnimState:Hide("HEAD")
-		owner.AnimState:Show("HEAD_HAIR")
-		owner.AnimState:Hide("HAIRFRONT")
-	end
-
+        owner.AnimState:Show("HEAD_HAIR")
+        owner.AnimState:Hide("HAIRFRONT")
+    end
 
     if inst.components.fueled ~= nil then
         inst.components.fueled:StartConsuming()
@@ -38,20 +50,17 @@ local function onunequip(inst, owner)
     owner.AnimState:Show("HAIR_NOHAT")
     owner.AnimState:Show("HAIR")
 
-    --[[if owner:HasTag("player") then
+    if owner:HasTag("player") then
        owner.AnimState:Show("HEAD")
-       owner.AnimState:Hide("HEAD_HAT")
-    end]]
-	
-	if owner:HasTag("player") then
-		owner.AnimState:Show("HEAD")
-		owner.AnimState:Hide("HEAD_HAIR")
-		owner.AnimState:Show("HAIRFRONT")
-	end
+       owner.AnimState:Hide("HEAD_HAIR")
+       owner.AnimState:Show("HAIRFRONT")
+    end
 
     if inst.components.fueled ~= nil then
 		inst.components.fueled:StopConsuming()
     end
+	
+	owner:RemoveEventCallback("locomote", equip_ness_check)
 end
 
 
@@ -69,6 +78,8 @@ local function fn()
 
     inst:AddTag("hat")
 	inst:AddTag("waterproofer")
+    if IsDLCEnabled(2) then MakeInventoryFloatable(inst, "anim", "anim") end
+
 
     inst:AddComponent("inventoryitem")
 	inst.inventory = inst.components.inventoryitem
@@ -82,8 +93,8 @@ local function fn()
     inst.components.equippable.equipslot = EQUIPSLOTS.HEAD
     inst.components.equippable:SetOnEquip(onequip)
     inst.components.equippable:SetOnUnequip(onunequip)
-        
-   --[[ inst.components.floater:SetSize("med")
+
+    --[[inst.components.floater:SetSize("med")
     inst.components.floater:SetVerticalOffset(0.1)
     inst.components.floater:SetScale(0.65)]]
 
@@ -91,27 +102,22 @@ local function fn()
 
     inst:AddComponent("fueled")
     inst.components.fueled.fueltype = "USAGE"
-    inst.components.fueled:InitializeFuelLevel(TUNING.BASEBALL_CAP_PERISHTIME)
+    inst.components.fueled:InitializeFuelLevel(30 * 16 * 12) --12 days
     inst.components.fueled:SetDepletedFn(inst.Remove)
 	
-    --inst:AddComponent("waterproofer")
-    --inst.components.waterproofer:SetEffectiveness(TUNING.WATERPROOFNESS_SMALL)
 	
 	inst:AddComponent("insulator")
-	--inst.components.insulator:SetInsulation(TUNING.INSULATION_SMALL)
-	inst.components.insulator.insulation = TUNING.INSULATION_SMALL
+	inst.components.insulator:SetInsulation(TUNING.INSULATION_SMALL)
 	inst.components.insulator:SetSummer()
 	
 	inst.components.equippable.insulated = true
-	
-	inst:AddTag("venting")
-	inst:AddTag("fogproof")
     return inst
 end
 STRINGS.NAMES.BASEBALL_CAP_NINTEN  = "Baseball Cap"
 STRINGS.CHARACTERS.GENERIC.DESCRIBE.BASEBALL_CAP_NINTEN = "It keeps the sun out of my eyes."
 STRINGS.CHARACTERS.WX78.DESCRIBE.BASEBALL_CAP_NINTEN = "THIS WILL COVER MY PROCESSING UNIT"
 STRINGS.CHARACTERS.GRAMNINTEN.DESCRIBE.BASEBALL_CAP_NINTEN = "My favorite baseball cap."
+
 
 return Prefab("baseball_cap_ninten", fn, assets)
 
