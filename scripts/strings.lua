3c89b48d0268da541d8ba21a9295bd6704764f81
Gamnraa <ltaz1466@gmail.com>
2025-03-27 20:34:13 -0400

    Create strings.lua

---
+++ scripts/strings.lua
@@ -0,0 +1,33 @@
+local STRINGS = GLOBAL.STRINGS
+
+STRINGS.CHARACTERS.GENERIC.DESCRIBE.INHALER_NINTEN = "Gives Ninten a fresh breath of air."
+
+STRINGS.CHARACTERS.WILLOW.DESCRIBE.INHALER_NINTEN = "He should probably take this back…"
+
+STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.INHALER_NINTEN = "I will be careful not to crush."
+
+STRINGS.CHARACTERS.WENDY.DESCRIBE.INHALER_NINTEN = "A temporary lift to a burden."
+
+STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.INHALER_NINTEN = "Medicine to help asthmatic people"
+
+STRINGS.CHARACTERS.WOODIE.DESCRIBE.INHALER_NINTEN = "We should get this back to the kiddo, eh Luce?"
+
+STRINGS.CHARACTERS.WAXWELL.DESCRIBE.INHALER_NINTEN = "I could give it back…I don’t know if I will yet."
+
+--STRINGS.CHARACTERS.WIGFRID.DESCRIBE.INHALER_NINTEN = "A small relic för the magical child" 
+
+STRINGS.CHARACTERS.WEBBER.DESCRIBE.INHALER_NINTEN = "We should keep it safe incase Ninten needs it!"
+
+STRINGS.CHARACTERS.WINONA.DESCRIBE.INHALER_NINTEN = "I’ll get this back to the kid, don’t need em’ dying on us."
+
+STRINGS.CHARACTERS.WARLY.DESCRIBE.INHALER_NINTEN = "Ninten without his inhaler is a recipe for disaster."
+
+STRINGS.CHARACTERS.WORTOX.DESCRIBE.INHALER_NINTEN = "An odd tool for certain mortals..."
+
+STRINGS.CHARACTERS.WORMWOOD.DESCRIBE.INHALER_NINTEN = "Thingy friend uses."
+
+STRINGS.CHARACTERS.WURT.DESCRIBE.INHALER_NINTEN = "Nin-ten makes funny noises without it, florp."
+
+STRINGS.CHARACTERS.WALTER.DESCRIBE.INHALER_NINTEN = "Hmmm, my handbook says an asthmatic person should never not be without their inhaler."
+
+STRINGS.CHARACTERS.WANDA.DESCRIBE.INHALER_NINTEN = "Timelines where he loses this never end well."
